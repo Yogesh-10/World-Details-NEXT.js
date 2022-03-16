3d9a7babf223f1393d36ca22f82f92b3c55322fc
Yogesh-10 <yogiyogesh100800@gmail.com>
2022-03-16 20:25:59 +0530

    bug fix

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import Layout from '../../components/Layout/Layout';
 import styles from './Country.module.css';
-import axios from 'axios';
+// import axios from 'axios';
 
 const getCountry = async (id) => {
 	const res = await fetch(`https://restcountries.eu/rest/v2/alpha/${id}`);
